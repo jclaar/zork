ae36bef7ee5c4c2102a6427ea4f22f8076a1f53a
jclaar <jclaar3@cox.net>
2024-11-26 19:46:55 -0800

    Made Flags use private inheritance

--- FlagSupport.h
+++ FlagSupport.h
@@ -2,21 +2,27 @@
 #include <boost/serialization/serialization.hpp>
 
 template <typename FlagType, size_t sz>
-class Flags
+class Flags : private std::bitset<sz>
 {
+    typedef std::bitset<sz> Base;
+    Flags(const Base& b) : Base(b)
+    {
+    }
 public:
-    typedef std::bitset<sz> FlagBits;
+    using Base::none;
+    using Base::any;
+    using Base::reference;
 
-    bool operator[](FlagType flag) const { return f[static_cast<int>(flag)]; }
-    typename FlagBits::reference operator[](FlagType flag) { return f[static_cast<int>(flag)]; }
-    bool test(FlagType flag) const { return f.test(static_cast<int>(flag)); }
-    bool none() const { return f.none(); }
-    bool any() const { return f.any(); }
-    Flags<FlagType, sz>& set() { f.set(); return *this; }
+    Flags() {}
+
+    bool operator[](FlagType flag) const { return Base::operator[](static_cast<int>(flag)); }
+    Base::reference operator[](FlagType flag) { return Base::operator[](static_cast<int>(flag)); }
+    bool test(FlagType flag) const { return Base::test(static_cast<int>(flag)); }
+    Flags<FlagType, sz>& set() { Base::set(); return *this; }
 
     Flags& flip(FlagType flag)
     {
-        f.flip(static_cast<int>(flag));
+        Base::flip(static_cast<int>(flag));
         return *this;
     }
 
@@ -24,11 +30,10 @@ public:
     template <class archive>
     void serialize(archive& ar, const unsigned int version)
     {
-        ar& f;
+        ar & static_cast<Base&>(*this);
     }
 
 private:
-    FlagBits f;
 
     template <typename FT, size_t Sz>
     friend Flags<FT, Sz> operator&(const Flags<FT, Sz>& lhs, const Flags<FT, Sz>& rhs);
@@ -37,7 +42,9 @@ private:
 template <typename FlagType, size_t sz>
 Flags<FlagType, sz> operator&(const Flags<FlagType, sz>& lhs, const Flags<FlagType, sz>& rhs)
 {
-    Flags<FlagType, sz> rv;
-    rv.f = lhs.f & rhs.f;
+    auto& fl = static_cast<const Flags<FlagType, sz>::Base&>(lhs);
+    auto& fr = static_cast<const Flags<FlagType, sz>::Base&>(rhs);
+    Flags<FlagType, sz> rv(fl & fr);
+    
     return rv;
 }
