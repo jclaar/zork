ab28dc66ae2c20f26506598f1f1e35ef0da3a500
Jeff Claar <jclaar3@cox.net>
2026-08-11 17:11:56 -0700

    Now uses proper std::nullopt.

--- memq.h
+++ memq.h
@@ -13,7 +13,7 @@ template <typename T, typename Container>
 MemqRet<Container> memq(const T& i, const Container& c)
 {
     typename Container::const_iterator iter = std::find(std::begin(c), std::end(c), i);
-    return iter == std::end(c) ? MemqRet<Container>() : MemqRet<Container>(iter);
+    return iter == std::end(c) ? std::nullopt : MemqRet<Container>(iter);
 }
 
 bool memq(const ObjectP& op, Iterator<ObjVector> ol);
