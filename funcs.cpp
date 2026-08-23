593c7da8aee2e8c2f53ad1528d39b86f17ac4c90
jclaar <jclaar3@cox.net>
2026-08-23 15:33:08 -0700

    Converted to new C++ random number generator.

--- funcs.cpp
+++ funcs.cpp
@@ -4,6 +4,7 @@
 #include "globals.h"
 #include "rooms.h"
 #include <vector>
+#include <random>
 #include <sstream>
 #include <chrono>
 #include <thread>
@@ -12,6 +13,8 @@
 class TtyBuff : public std::basic_stringbuf<char, std::char_traits<char>>
 {
 public:
+	TtyBuff() : gen(std::random_device{}()), dist(50, 200) {}
+
     bool IsTerminal() const { return term_sim; }
     void SetTerminal(bool on) { term_sim = on; }
 
@@ -19,11 +22,13 @@ protected:
     int sync() override
     {
         using namespace std::chrono_literals;
+        // Delay between each character in terminal mode.
         auto delay = term_sim ? 10ms : 0ms;
         char c;
         if (term_sim)
         {
-            std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 200));
+            // If in terminal mode, pause for a little bit to simulate memory/disk access, etc.
+            std::this_thread::sleep_for(std::chrono::milliseconds(dist(gen)));
         }
         while ((c = this->sbumpc()) != (char) traits_type::eof())
         {
@@ -33,6 +38,7 @@ protected:
                 std::cout.flush();
                 std::this_thread::sleep_for(delay);
             }
+			// If scripting, write this character to the script channel as well.
             if (script_channel)
             {
                 (*script_channel) << c;
@@ -44,6 +50,8 @@ protected:
     }
 
 private:
+    std::mt19937 gen;                               // random engine
+    std::uniform_int_distribution<int> dist;        // distribution
     bool term_sim = false;
 };
 
